2a89db01bc15e1587467ebb01923f24cdae928ca
Paul Butcher <paul@paulbutcher.com>
2011-10-24 23:07:17 +0100

    Step 2

--- src/generate-mocks/scala/GenerateMocks.scala
+++ src/generate-mocks/scala/GenerateMocks.scala
@@ -3,4 +3,5 @@ package com.example
 import org.scalamock.annotation.mock
  
 @mock[Turtle]
+@mock[ColouredTurtle]
 class Dummy
---
+++ src/test/scala/ColouredTurtleTest.scala
@@ -0,0 +1,21 @@
+package com.example
+ 
+import org.scalatest.FunSuite
+import org.scalamock.scalatest.MockFactory
+import org.scalamock.generated.GeneratedMockFactory
+ 
+class ColouredTurtleTest extends FunSuite with MockFactory with GeneratedMockFactory {
+ 
+  test("coloured turtles") {
+    val m1 = mock[ColouredTurtle]
+    val m2 = mock[ColouredTurtle]
+ 
+    m1.expects.newInstance(1.0, 0.0, 0.0)
+    m2.expects.newInstance(0.0, 1.0, 0.0)
+    m2.expects.forward(3.0)
+ 
+    val t1 = ColouredTurtle('red)
+    val t2 = ColouredTurtle('green)
+    t2.forward(3.0)
+  }
+}
