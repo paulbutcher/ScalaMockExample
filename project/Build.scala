b872cd87a9cdd3e40f3b995b996d371196fbf676
Paul Butcher <paul@paulbutcher.com>
2011-11-27 22:55:17 +0000

    Update to ScalaMock 2.1

--- project/Build.scala
+++ project/Build.scala
@@ -10,9 +10,9 @@ object MyBuild extends Build {
     scalaVersion := "2.9.1",
  
     resolvers += ScalaToolsSnapshots,
-    libraryDependencies += "org.scalamock" %% "scalamock-scalatest-support" % "2.0-SNAPSHOT",
+    libraryDependencies += "org.scalamock" %% "scalamock-scalatest-support" % "2.1",
     autoCompilerPlugins := true,
-    addCompilerPlugin("org.scalamock" %% "scalamock-compiler-plugin" % "2.0-SNAPSHOT"))
+    addCompilerPlugin("org.scalamock" %% "scalamock-compiler-plugin" % "2.1"))
  
   lazy val myproject = Project("MyProject", file(".")) settings(generateMocksSettings: _*) configs(Mock)
 }
