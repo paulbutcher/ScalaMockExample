70131247df4811ce19b7a32c230f7c07beb62507
Paul Butcher <paul@paulbutcher.com>
2012-04-17 12:01:22 +0100

    Add new Sonatype repo

--- project/Build.scala
+++ project/Build.scala
@@ -9,6 +9,7 @@ object ScalaMockExample extends Build {
     version := "1.0",
     scalaVersion := "2.9.1",
  
+	resolvers += "Sonatype OSS Snapshots" at "http://oss.sonatype.org/content/repositories/snapshots/",
     libraryDependencies += "org.scalamock" %% "scalamock-scalatest-support" % "2.3-SNAPSHOT",
     autoCompilerPlugins := true,
     addCompilerPlugin("org.scalamock" %% "scalamock-compiler-plugin" % "2.3-SNAPSHOT"))
