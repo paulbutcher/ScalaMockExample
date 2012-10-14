5a64c098830008772f1393a4c5a58946a578a566
Paul Butcher <paul@paulbutcher.com>
2012-10-14 19:04:06 +0100

    Add Mockito style version

--- build.sbt
+++ build.sbt
@@ -6,4 +6,5 @@ scalaVersion := "2.10.0-RC1"
 
 scalacOptions ++= Seq("-deprecation", "-unchecked")
 
-libraryDependencies += "org.scalamock" % "scalamock-scalatest-support_2.10.0-RC1" % "3.0-M4"
+libraryDependencies +=
+  "org.scalamock" % "scalamock-scalatest-support_2.10.0-RC1" % "3.0-M5"
---
+++ src/test/scala/com/example/ControllerTestMockitoStyle.scala
@@ -0,0 +1,31 @@
+package com.example
+ 
+import org.scalatest.FunSuite
+import org.scalamock.scalatest.MockFactory
+import scala.math.{Pi, sqrt}
+ 
+class ControllerTestMockitoStyle extends FunSuite with MockFactory {
+ 
+  test("draw line") {
+    val mockTurtle = stub[Turtle]
+    val controller = new Controller(mockTurtle)
+ 
+    inSequence {
+      inAnyOrder {
+        (mockTurtle.getPosition _) when () returns (0.0, 0.0)
+        (mockTurtle.getAngle _) when () returns 0.0 once
+      }
+      (mockTurtle.getAngle _) when () returns Pi / 4
+    }
+ 
+    controller.drawLine((1.0, 1.0), (2.0, 1.0))
+    
+    inSequence {
+      (mockTurtle.turn _) verify ~(Pi / 4)
+      (mockTurtle.forward _) verify ~sqrt(2.0)
+      (mockTurtle.turn _) verify ~(-Pi / 4)
+      (mockTurtle.penDown _) verify ()
+      (mockTurtle.forward _) verify 1.0
+    }
+  }
+}
